75c920be0281e1972e2e7520816f9ed55f4ae244
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2022-08-31 18:40:24 -0300

    Remove CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,39 +1,39 @@
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
-import { downloadSourceCode } from "./tools/explorer";
-import { slither as runSlither } from "./tools/slither";
-import { md5 as md5sum } from "./tools/md5";
-import { findSolidityVersion, selectSolidityVersion } from "./tools/solc";
-import database, { IReport } from "./database";
+// import { downloadSourceCode } from "./tools/explorer";
+// import { slither as runSlither } from "./tools/slither";
+// import { md5 as md5sum } from "./tools/md5";
+// import { findSolidityVersion, selectSolidityVersion } from "./tools/solc";
+// import database, { IReport } from "./database";
 
-export async function cli(): Promise<void> {
-  await database.connect();
-  const argv = require("minimist")(process.argv.slice(2), {
-    string: ["contract"],
-  });
-  const { explorer, contract, download, slither } = argv;
+// export async function cli(): Promise<void> {
+//   await database.connect();
+//   const argv = require("minimist")(process.argv.slice(2), {
+//     string: ["contract"],
+//   });
+//   const { explorer, contract, download, slither } = argv;
 
-  if (download) {
-    await downloadSourceCode(explorer, contract, `/tmp/${contract}`);
-  }
-  if (slither) {
-    const md5 = await md5sum(`/tmp/${contract}`);
-    console.log({ md5 });
-    const report = await database.Report.findOne({ contract, explorer, md5 });
-    if (!report) {
-      const version = await findSolidityVersion(`/tmp/${contract}`);
-      await selectSolidityVersion(version);
-      const details = await runSlither(`/tmp/${contract}`);
-      await database.Report.create<IReport>({
-        contract,
-        md5,
-        explorer,
-        details,
-        tool: "slither",
-      });
-    }
-  }
-  console.log("Done");
-  process.exit(0);
-}
+//   if (download) {
+//     await downloadSourceCode(explorer, contract, `/tmp/${contract}`);
+//   }
+//   if (slither) {
+//     const md5 = await md5sum(`/tmp/${contract}`);
+//     console.log({ md5 });
+//     const report = await database.Report.findOne({ contract, explorer, md5 });
+//     if (!report) {
+//       const version = await findSolidityVersion(`/tmp/${contract}`);
+//       await selectSolidityVersion(version);
+//       const details = await runSlither(`/tmp/${contract}`);
+//       await database.Report.create<IReport>({
+//         contract,
+//         md5,
+//         explorer,
+//         details,
+//         tool: "slither",
+//       });
+//     }
+//   }
+//   console.log("Done");
+//   process.exit(0);
+// }
